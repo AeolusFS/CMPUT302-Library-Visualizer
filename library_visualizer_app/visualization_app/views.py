2f3e27be11e772e94acde8fd727d664f6e1bf20a
Storm Kaefer <stormkaefer@gmail.com>
2018-03-16 03:33:08 -0600

    added bokeh code, able to actually see graph now!

--- library_visualizer_app/visualization_app/views.py
+++ library_visualizer_app/visualization_app/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from bokeh.plotting import figure, output_file, show 
+from bokeh.embed import components
 
 def index(request):
+    x= [1,3,5,7,9,11,13]
+    y= [1,2,3,4,5,6,7]
+    title = 'y = f(x)'
+
+    plot = figure(title= title , 
+        x_axis_label= 'X-Axis', 
+        y_axis_label= 'Y-Axis', 
+        plot_width =400,
+        plot_height =400)
+
+    plot.line(x, y, legend= 'f(x)', line_width = 2)
+    #Store components 
+    script, div = components(plot)
+
+    #Feed them to the Django template.
     return render(request, 'visualization_app/index.html', {
-        # Insert data here, so template can render it
+        'script' : script , 
+        'div' : div
     })
+
