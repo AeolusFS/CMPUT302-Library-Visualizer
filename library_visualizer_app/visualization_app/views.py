d863d1282648005fad55dda6cfa42fcbeab9c3b6
Storm Kaefer <stormkaefer@gmail.com>
2018-04-02 23:43:30 -0600

    got working navbars, and reduced confusing code in the html files

--- library_visualizer_app/visualization_app/views.py
+++ library_visualizer_app/visualization_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.urls import reverse
 
 from bokeh.plotting import figure, output_file, show 
 from bokeh.embed import components
@@ -26,23 +26,32 @@ def visualization(request):
         print(request.POST) # The library names should appear in the request dictionary
         # Check with libraries are here, then display them. If empty, well dang, the person submitted an empty form. Display nothing
 
-    x= [1,3,5,7,9,11,13]
-    y= [1,2,3,4,5,6,7]
-    title = 'y = f(x)'
+        # List of all the different visualization names
+        visualization_names = ["Popularity Count", "Release Frequency", "Last Modified Date", \
+        "Backwards Compatibility", "Stack Overflow", "Security & Performance", "Issue Data Response Time", "Issue Data Resolved Time"]
 
-    plot = figure(title= title , 
-        x_axis_label= 'X-Axis', 
-        y_axis_label= 'Y-Axis', 
-        plot_width =400,
-        plot_height =400)
 
-    plot.line(x, y, legend= 'f(x)', line_width = 2)
-    #Store components 
-    script, div = components(plot)
+        x= [1,3,5,7,9,11,13]
+        y= [1,2,3,4,5,6,7]
+        title = 'y = f(x)'
 
-    #Feed them to the Django template.
-    return render(request, 'visualization_app/visualizations.html', {
-        'script' : script , 
-        'div' : div
-    })
+        plot = figure(title= title , 
+            x_axis_label= 'X-Axis', 
+            y_axis_label= 'Y-Axis', 
+            plot_width =400,
+            plot_height =400)
+
+        plot.line(x, y, legend= 'f(x)', line_width = 2)
+        #Store components 
+        script, div = components(plot)
+
+        #Feed them to the Django template.
+        return render(request, 'visualization_app/visualizations.html', {
+            'script' : script, 
+            'div' : div,
+            'visualization_names' : visualization_names,
+        })
+    
+    else:
+        return redirect('/') # Go back to main screen
 
