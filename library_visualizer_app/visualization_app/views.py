3822c06d77bd7d4a221bb1d453b74213bbbe7aa4
DESKTOP-I5QCLVS\Zakia <saklain@ualberta.ca>
2018-04-04 02:53:31 -0600

    Added all the libraries in view.py

--- library_visualizer_app/visualization_app/views.py
+++ library_visualizer_app/visualization_app/views.py
@@ -10,8 +10,17 @@ def index(request):
     # pass in each library as an array to the template
     # library data can be made in another py file and imported here
     testing_libraries = [lib_info['testng'], lib_info['junit4']]
-    mock_libraries = [lib_info['mockito']]
-    all_libraries = [testing_libraries, mock_libraries]
+    logging_libraries = [lib_info['slf4j'], lib_info['log4j2'], lib_info['logback'], lib_info['commons logging'], lib_info['tinylog'], lib_info['blitz4j'], lib_info['minlog']]
+    utilities_libraries = [lib_info['guava'], lib_info['commons lang']]
+    mocking_libraries = [lib_info['mockito'], lib_info['easymock'], lib_info['powermock'], lib_info['jmock']]
+    cryptography_libraries = [lib_info['bouncycastle'], lib_info['commons crypto'], lib_info['conceal'], lib_info['chimera'], lib_info['spongycastle'], lib_info['keyczar'], lib_info['conscrypt']]
+    json_libraries = [lib_info['gson'], lib_info['json.simple']]
+    databases_libraries = [lib_info['h2'], lib_info['derby']]
+    security_libraries = [lib_info['shiro'], lib_info['spring security']]
+    ormapping_libraries = [lib_info['hibernate orm'], lib_info['mybatis3'], lib_info['ormlite']]
+    xml_libraries = [lib_info['xerces2-j'], lib_info['dom4j'], lib_info['jdom']]
+
+    all_libraries = [testing_libraries, logging_libraries, utilities_libraries, mocking_libraries, cryptography_libraries, json_libraries, databases_libraries, security_libraries, ormapping_libraries, xml_libraries]
 
     return render(request, 'visualization_app/main_page.html', {
         "all_libraries" : all_libraries,
@@ -24,15 +33,93 @@ def visualization(request):
 
         # Do this for all libraries
         compare_libraries = []
+        #testing_libraries
         if 'junit4' in request.POST:
             compare_libraries.append(lib_info['junit4'])
-        if 'testinglibrary' in request.POST:
-            compare_libraries.append(lib_info['testinglibrary'])
         if 'testng' in request.POST:
             compare_libraries.append(lib_info['testng'])
+
+        #logging_libraries
+        if 'slf4j' in request.POST:
+            compare_libraries.append(lib_info['slf4j'])
+        if 'log4j2' in request.POST:
+            compare_libraries.append(lib_info['log4j2'])
+        if 'logback' in request.POST:
+            compare_libraries.append(lib_info['logback'])
+        if 'commons logging' in request.POST:
+            compare_libraries.append(lib_info['commons logging'])
+        if 'tinylog' in request.POST:
+            compare_libraries.append(lib_info['tinylog'])
+        if 'blitz4j' in request.POST:
+            compare_libraries.append(lib_info['blitz4j'])
+        if 'minlog' in request.POST:
+            compare_libraries.append(lib_info['minlog'])
+
+        #utilities_libraries
+        if 'guava' in request.POST:
+            compare_libraries.append(lib_info['guava'])
+        if 'commons lang' in request.POST:
+            compare_libraries.append(lib_info['commons lang'])
+
+        #mocking_libraries
         if 'mockito' in request.POST:
             compare_libraries.append(lib_info['mockito'])
-        # Add more here for more libraries
+        if 'easymock' in request.POST:
+            compare_libraries.append(lib_info['easymock'])
+        if 'powermock' in request.POST:
+            compare_libraries.append(lib_info['powermock'])
+        if 'jmock' in request.POST:
+            compare_libraries.append(lib_info['jmock'])
+
+        #cryptography_libraries
+        if 'bouncycastle' in request.POST:
+            compare_libraries.append(lib_info['bouncycastle'])
+        if 'commons crypto' in request.POST:
+            compare_libraries.append(lib_info['commons crypto'])
+        if 'conceal' in request.POST:
+            compare_libraries.append(lib_info['conceal'])
+        if 'chimera' in request.POST:
+            compare_libraries.append(lib_info['chimera'])
+        if 'spongycastle' in request.POST:
+            compare_libraries.append(lib_info['spongycastle'])
+        if 'keyczar' in request.POST:
+            compare_libraries.append(lib_info['keyczar'])
+        if 'conscrypt' in request.POST:
+            compare_libraries.append(lib_info['conscrypt'])
+
+        #json_libraries
+        if 'gson' in request.POST:
+            compare_libraries.append(lib_info['gson'])
+        if 'json.simple' in request.POST:
+            compare_libraries.append(lib_info['json.simple'])
+
+        #databases_libraries
+        if 'h2' in request.POST:
+            compare_libraries.append(lib_info['h2'])
+        if 'derby' in request.POST:
+            compare_libraries.append(lib_info['derby'])
+
+        #security_libraries
+        if 'shiro' in request.POST:
+            compare_libraries.append(lib_info['shiro'])
+        if 'spring security' in request.POST:
+            compare_libraries.append(lib_info['spring security'])
+
+        #ormapping_libraries
+        if 'hibernate orm' in request.POST:
+            compare_libraries.append(lib_info['hibernate orm'])
+        if 'mybatis3' in request.POST:
+            compare_libraries.append(lib_info['mybatis3'])
+        if 'ormlite' in request.POST:
+            compare_libraries.append(lib_info['ormlite'])
+
+        #xml_libraries
+        if 'xerces2-j' in request.POST:
+            compare_libraries.append(lib_info['xerces2-j'])
+        if 'dom4j' in request.POST:
+            compare_libraries.append(lib_info['dom4j'])
+        if 'jdom' in request.POST:
+            compare_libraries.append(lib_info['jdom'])
 
         # List of all the different visualization names
         visualizations = [["Popularity Count"], ["Release Frequency"], ["Last Modified Date"], \
